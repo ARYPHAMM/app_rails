762520ee7855718cfe454ddff3e1dfc8736b9a65
ARYPHAMM <quoctienphamm@gmail.com>
2019-01-31 20:55:04 +0700

    users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+def index
+ @users = User.all
+end
+
 def new
    @user = User.new
 end
@@ -7,9 +11,42 @@ end
 def create
    # render plain: params[:user].inspect
    @user = User.new(user_params)
-   @user.save
-   redirect_to users_show(@user)
+   if @user.save
+   	   flash[:nontice] = "User was successfully"
+       redirect_to user_path (@user)
+   else
+   	render 'new'
+   end
+   	#@user.save
+   # redirect_to users_show(@user)
 end
+
+def destroy
+	@user = User.find(params[:id])
+	@user.destroy
+	flash[:nontice] = " User was destroy successfully " 
+	redirect_to users_path
+end
+
+def show
+  @user = User.find(params[:id])
+end
+
+def edit
+  @user = User.find(params[:id])
+end 
+
+def update
+ @user = User.find(params[:id])
+   if @user.update(user_params)
+   	   flash[:nontice] = "User was successfully"
+       redirect_to user_path (@user)
+   else
+   	render 'new'
+   end
+end
+
+	
 
  private
    def user_params
